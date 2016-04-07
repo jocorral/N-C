f6dd55bd6107337a47671ad1d9d93a07a992572a
jocorral <jorgecrrl@gmail.com>
2016-04-07 20:00:38 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,14 +7,55 @@
 
 #include <stdio.h>
 
+
 int main(void){
-	int dec1;
+	char dec1;
+	char *usuario;
+	usuario = (char*)malloc(21*sizeof(char));
+	char *contrasenya;
+	char *contrasenya2;
+	char *nombre;
+	char *apellido;
+
 	printf("Hola, bienvenido a N&C \n"
 			"Por favor, elija una opción:\n"
 			"1. Log in\n"
-			"2. Crear usuario");
+			"2. Crear usuario\n");
 	fflush(stdout);
 	dec1 = getchar();
-	printf("%i", dec1);
+	switch(dec1){
+	case '1':
+		printf("Introduzca usuario y contraseña:\n"
+				"Usuario:\n");
+		fflush(stdout);
+		fgets(usuario, 21, stdin);
+		printf("%c", usuario[0]);
+
+		printf("Contraseña:\n");
+		fflush(stdout);
+
+		//Coger contraseña
+
+
+		break;
+	case '2':
+		printf("CREACIÓN DE NUEVO USUARIO\n--------------------------\nIntroduzca su nombre:\n");
+		fflush(stdout);
+
+		printf("Primer apellido:\n");
+		fflush(stdout);
+
+		printf("Introduzca nombre de usuario:\n");
+		fflush(stdout);
+
+		printf("Introduzca contraseña:\n");
+		fflush(stdout);
+
+		printf("Repita la contraseña:\n");
+		fflush(stdout);
+
+		break;
+	}
+
 	return 0;
 }
